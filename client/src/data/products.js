aeebc1fc22204f2821d08907cae6fa13a34c6eca
Ridhi chawla <ridhichawla2003@gmail.com>
2026-06-25 11:43:33 +0530

    Fix case sensitive imports

--- client/src/data/products.js
+++ client/src/data/products.js
@@ -1,35 +1,35 @@
-import img1 from "../pages/images/Wallet.png";
-import img2 from "../pages/images/formalbelt.png";
-import img3 from "../pages/images/Reversible.png";
-const products = [
+// import img1 from "../pages/images/Wallet.png";
+// import img2 from "../pages/images/formalbelt.png";
+// import img3 from "../pages/images/Reversible.png";
+// const products = [
 
-{
- id:1,
- category:"men",
- name:"Leather Wallet",
- price:600,
- image:img1,
- description:"Premium Wallet"
-},
+// {
+//  id:1,
+//  category:"men",
+//  name:"Leather Wallet",
+//  price:600,
+//  image:img1,
+//  description:"Premium Wallet"
+// },
 
-{
- id:2,
- category:"Men",
- name:"Reversible Belt",
- price:800,
- image:img3,
- description:"Premium Belt"
-},
+// {
+//  id:2,
+//  category:"Men",
+//  name:"Reversible Belt",
+//  price:800,
+//  image:img3,
+//  description:"Premium Belt"
+// },
 
-{
- id:3,
- category:"Men",
- name:"Formal Belt",
- price:600,
- image:img2,
- description:"Premium formal belt"
-}
+// {
+//  id:3,
+//  category:"Men",
+//  name:"Formal Belt",
+//  price:600,
+//  image:img2,
+//  description:"Premium formal belt"
+// }
 
-];
+// ];
 
-export default products;
+// export default products;
